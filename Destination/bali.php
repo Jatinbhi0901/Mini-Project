7c5152187821f987550af05d0f91d9045c551bc6
Mansi Bhondave <mansibhondave@gmail.com>
2022-05-18 16:21:05 +0530

    1 destination added

---
+++ Destination/bali.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/navigation.css" />
+    <link rel="stylesheet" href="../css/bali.css">
+</head>
+
+<body>
+    <?php 
+        include("../navigation.php")
+    ?>
+
+    <section class="section1">
+        <div class="container1"></div>
+
+        <div class="container2">
+            <h1>Bali Tour</h1>
+            <h4>3 Nights-4 Days</h4>
+            <div class="line-3">
+                <hr />
+                <h3>Inclusions</h3>
+                <div class="b1">
+                    <div>
+                        <img src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/64/000000/external-hotel-hotel-kiranshastry-gradient-kiranshastry.png"
+                            width="50px" />
+                        <br />
+
+                        <h5>5 star</h5>
+                    </div>
+
+                    <div class="b2">
+                        <img src="https://img.icons8.com/nolan/64/car-roof-box.png" width="50px" />
+                        <br />
+                        <h5>Transfer</h5>
+                    </div>
+
+                    <div class="b3">
+                        <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-dinner-casino-flaticons-lineal-color-flat-icons.png"
+                            width="50px" />
+                        <br />
+                        <h5>Meals</h5>
+                    </div>
+
+                    <div class="b4">
+                        <img src="https://img.icons8.com/external-colours-bomsymbols-/91/000000/external-binoculars-digital-design-colors-set-1-colours-bomsymbols-.png"
+                            width="50px" />
+                        <br />
+                        <h5>Sightseeing</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <br />
+    <section class="section2">
+        <div class="box">
+            <h2 class="overview">Overview</h2>
+            <p>
+                With changing times, one thing that stayed constant about Bali is the love of its loyal travellers
+                towards its beauty. And this fact seems truly natural as not only this destination has given 
+                reasons to solo travellers to travel often for its beaches, verdant greenery, art forms, spirituality 
+                but also to the couples who look for such a perfectly compact tourism destination in Indonesia. Giving 
+                your perfect honeymoon tour search the perfect solution is Tour My India and its exciting range of Bali
+                honeymoon packages. And our 3N/4D Bali Itinerary is the best way to justify our dedication towards 
+                making your holidays with your loved ones most memorable. Through our holiday packages, we take you for
+                a tour to only the most renowned places of Bali like its villages namely Batubulan, Celuk, Batuan and 
+                Mas that are popular for nurturing one form of art or the other. Furthermore, we also cover must visit 
+                places in Bali like Tirta Empul Holy Spring Temple, Wanara Wana Monkey Forest to name a few. 
+                And lastly, end the tour on a relaxing note with a class apart spa experience.
+                <br>
+        </div>
+    </section>
+    <br>
+    <section class="section3">
+        <div class="box">
+            <h2 class="overview">Itinerary</h2>
+            <h3 class="day">Day1:</h3>
+            <u>
+                <h4> ARRIVAL AT BALI INTERNATIONAL AIRPORT</h4>
+            </u>
+            <p>
+                Upon your arrival at Bali International Airport, our driver will be there for pickup and will 
+                escort you to your private villa. Rest of the day is at your disposal.
+
+            </p>
+            <br>
+            <div class="line">
+            </div>
+            <br>
+            <h3 class="day">Day2:</h3>
+            <u>
+                <h4> BALI SIGHTSEEING TOUR</h4>
+            </u>
+            <p>
+
+                Once you are done with finishing every bite of your breakfast with your partner, the first tourist
+                attraction that we will be covering is Batubulan village where you will get to enjoy the Magical 
+                Show of Barong Keris Dance. Followed by a visit to Tohpati village which is known for the traditionally 
+                blessed Batik center; Celuk Village renowned for silversmiths; Batuan Village for its home painter artist 
+                community, and Mas village for housing Bali’s famous wooden sculpture art. Thereafter we will head to 
+                sightsee the Tegenungan Waterfall and after some time proceed to Tirta Empul Holy Spring Temple and whiff 
+                the effervescent spices garden of Temen Village. Later we will head to Kintamani where you can behold the 
+                sight of Batur Volcano that overlooks the Crater Lake and refill our stomach at a local restaurant in the 
+                afternoon. On the way back, you can stop buy at the rice terraces of Tegalalang in Ubud and then drive to 
+                the town and pay a short visit to Wanara Wana Monkey Forest. This is followed by strolling around the Ubud 
+                Center and Palace. After the tiring day you can relax and unwind at your villa as late night you can take 
+                your partner for dinner at Jimbaran Bay.
+
+
+            </p>
+            <br>
+            <div class="line">
+            </div>
+            <br>
+            <h3 class="day">Day3:</h3>
+            <u>
+                <h4>TURTLE ISLAND TOUR AND SPA TREATMENT</h4>
+            </u>
+            <p>
+                Rise and shine as not only some toothsome breakfast waits for you but a tour to turtle island on 
+                a glass bottom boat. One can choose to indulge in water activities like jet skiing, parasailing, 
+                banana boat ride (at your own expense). Post that you will head for a spa experience where you 
+                can take pleasure in services like four hand massage, traditional facial, hair cream bath, 
+                Shirodhara aromatic foot bath, warm stone massage, luxury flower bath, body lotion. Along with 
+                that light meals and juice will be served during the treatment. Later you will proceed to your 
+                villa to spend the night with your partner or explore Bali.
+
+            </p>
+            <br>
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day4:</h3>
+            <u>
+                <h4> DEPARTURE </h4>
+            </u>
+            <p>
+
+                Start your day with revelling in some delectable breakfast at the villa post which you will be 
+                escorted to the Airport.
+
+            </p>
+
+            <br>
+
+        </div>
+    </section>
+    <form action="">
+        <button type="submit" class="trip">Book Now</button>
+    </form>
+</body>
+
+</html>
