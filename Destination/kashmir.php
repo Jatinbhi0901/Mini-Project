ce2bc929d1405bfdb093f7a16f3969c0d7ccb937
Jatin <jatinbhirud09@gmail.com>
2022-05-18 11:02:21 +0530

    navbar

---
+++ Destination/kashmir.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/kashmir.css" />
+    <link rel="stylesheet" href="../css/navigation.css">
+</head>
+
+<body>
+    <?php 
+    include("../navigation.php")
+    ?> 
+    <section class="section1">
+        <div class="container1"></div>
+
+        <div class="container2">
+            <h1>Best of Kashmir</h1>
+            <h4>5 Nights-6 Days</h4>
+            <div class="line-3">
+                <hr />
+                <h3>Inclusions</h3>
+                <div class="b1">
+                    <div>
+                        <img src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/64/000000/external-hotel-hotel-kiranshastry-gradient-kiranshastry.png"
+                            width="50px" />
+                        <br />
+
+                        <h5>3 star</h5>
+                    </div>
+
+                    <div class="b2">
+                        <img src="https://img.icons8.com/nolan/64/car-roof-box.png" width="50px" />
+                        <br />
+                        <h5>Transfer</h5>
+                    </div>
+
+                    <div class="b3">
+                        <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-dinner-casino-flaticons-lineal-color-flat-icons.png"
+                            width="50px" />
+                        <br />
+                        <h5>Meals</h5>
+                    </div>
+
+                    <div class="b4">
+                        <img src="https://img.icons8.com/external-colours-bomsymbols-/91/000000/external-binoculars-digital-design-colors-set-1-colours-bomsymbols-.png"
+                            width="50px" />
+                        <br />
+                        <h5>Slightseeing</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <br />
+    <section class="section2">
+        <div class="box">
+            <h2 class="overview">Overview</h2>
+            <p>
+                The valley of Kashmir is popular amongst tourists who come here for a
+                holiday from across the globe. To offer visitors a memorable
+                experience of Kashmir tour, we offer 5N/6D Best of Kashmir tour
+                package for those planning a family vacation or honeymoon tour.</p>
+
+            <p> This Kashmir holiday package includes sightseeing at the top destinations
+                like Srinagar, Sonmarg, Gulmarg and Pahalgam. The 5 nights 6 days Best
+                of Kashmir holiday package shall begin with a tour of Srinagar and its
+                attractions like Mughal Gardens, Dal lake and Shankaracharya Temple.
+                Enjoy a houseboat stay in Srinagar. </p>
+            <p>The next destination in the travel
+                itinerary takes you to the popular hill station of Gulmarg which is
+                known for its cable car ride. The last destination of this amazing
+                Kashmir sightseeing tour package is Pahalgam which is known for its
+                lush meadows and valleys like Aru and Betaab which you will visit
+                along with rocky valley of Chandanwadi and Snow Point.</p>
+            </p>
+            <br>
+        </div>
+    </section>
+    <br>
+    <section class="section3">
+        <div class="box">
+            <h2 class="overview">Itinerary</h2>
+            <h3 class="day">Day1:</h3>
+            <u>
+                <h4>Arrive Srinagar - Local Sightseeing</h4>
+            </u>
+            <p>
+                Welcome to Srinagar, the Lake City. On arrival at the Srinagar Airport. you will be received by Tour My
+                India’s representative / driver.
+
+            <p> After the check- in, proceed for sightseeing in Srinagar. Visit the world-famous Mughal Gardens - Nishat
+                Bagh (The garden of pleasure), Shalimar Bagh (Abode of love), and Chasme Shahi Garden, and also pay
+                visit to the Shankaracharya Temple.</p>
+
+            <p>Later in the evening proceed for shikara riding in Dal lake.</p>
+
+            <p>Overnight Srinagar Houseboat.
+            </p>
+            <p>
+                <b> Nishat Garden :</b> The second-largest Mughal Garden in Kashmir, Nishat sits on the eastern side of
+                Dal Lake and is a
+                must-visit place to add to the sightseeing tour. A terraced garden, Nishat Bagh was built in 1633 AD by
+                Asaf Khan, brother of Mughal Empress, Noor Jahan.
+            </p>
+
+            <p><b> Shalimar Garden :</b> The Shalimar Bagh is the largest of the three Mughal Gardens in Srinagar. The
+                beautiful garden was commissioned by Mughal Emperor Jahangir in 1619 for his wife Noor Jahan. Also
+                called the Garden of Delight, Shalimar Bagh is a three-terraced garden and its main highlight is Chini
+                Khanas or arched niches.</p>
+
+            <p><b>Chashme Shahi :</b> The smallest of the three Mughal Gardens in Srinagar, Chashme Shahi is primarily
+                famous
+                for its cold water spring which is believed to have medicinal properties. This popular garden was built
+                by Mughal Emperor Shah Jahan as a gift for his elder son, Dara Shikoh.</p>
+
+            <p><b>About Srinagar :</b>The summer capital, Srinagar is known for its natural beauty, rich history, and
+                unique
+                experience of staying a houseboat. The city is home to the sacred shrine of Hazratbal, where it is
+                believed the hair strand of Prophet Mohammad is preserved. Apart from this, Srinagar is famous for
+                shikara rides of Dal Lake, Mughal Gardens, Pari Mahal, and Hari Parbat Fort.
+            </p>
+            <br>
+            <div class="line">
+            </div>
+            <br>
+            <h3 class="day">Day2:</h3>
+            <u>
+                <h4>Srinagar – Excursion to Sonmarg (90 Kms / Approx 03 Hours Drive- One Way)</h4>
+            </u>
+            <p>
+                After the breakfast at houseboat/hotel, leave for day excursion to Sonmarg, which is a beautiful drive
+                from Srinagar. On the way, you will stop at many beautiful spots. Continue driving to Sonamarg 2690 Mtrs
+                (Meadow of Gold).</p>
+
+            <p> One can enjoy <b> horse riding</b> to visit <b> Thajiwas Glacier</b> where snow remains round the year.
+                Sonamarg is also
+                known as Gateway to Ladakh. In the evening return to Srinagar for overnight stay at hotel.</p>
+
+            <p><b>About Sonmarg : </b> The ‘Meadow of Gold,’ Sonmarg boasts scenic beauty of nature. Sonmarg’s top
+                attraction
+                is Thajiwas Glacier, where can either trek or take a pony to reach. Apart from this, Sonmarg is known as
+                the base for many treks to remote villages like Naranag and alpine lakes like Vishansar and Krishansar.
+                It is an ideal place for trout fishing and river rafting as well.
+            </p>
+            <br>
+            <div class="line">
+            </div>
+            <br>
+            <h3 class="day">Day3:</h3>
+            <u>
+                <h4>Srinagar to Gulmarg (60 km / Approx 2-3 Hours Drive)</h4>
+            </u>
+            <p>
+                After the breakfast in the morning, we will begin our countryside tour towards Gulmarg. We will drive
+                past colorful villages and rice fields that will give you an insight into the rich culture of Kashmir.
+            </p>
+
+            <p><b>Gulmarg :</b> Gulmarg is one of the top hill stations in India and is majorly known to have world’s
+                highest
+                golf course. It’s major attraction is Gulmarg Gondola, which is Asia’s largest cable car project. The
+                beautiful destination in Kashmir is also quite popular for skiing and snowboarding in the winter season.
+            </p>
+
+            <p>The 08 minutes of ropeway ride on Gulmarg Gondola will take you past colorful shepherd hutments to the
+                lower ridge of the Apharwat Peak. From here, you can click lots of pictures. After spending time here,
+                we will descend to Gulmarg base after an hour. overnight at hotel.
+            </p>
+            <br>
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day4:</h3>
+            <u>
+                <h4>Gulmarg to Pahalgam (140 km / Approx 04 Hours Drive)</h4>
+            </u>
+            <p>After the breakfast, a drive to Pahalgam. Known as ‘The valley of Shepherds or Trekker's Paradise,’
+                Pahalgam (2133 mts ) is one of the most popular tourist places in Kashmir and the top preference for
+                Indian Film Industry.</p>
+
+            <p> En route, visit Saffron fields, Awantipur ruins (1100-yr-old temple of Lord Vishnu built by King
+                Awantiverma), the beauty of the countryside and lots of rice fields.</p>
+
+            <p>Arrive Pahalgam and transfer to the hotel. After the lunch, take a walk around and click pictures of the
+                magnificent views of mountains.</p>
+
+            <p>Dinner and Overnight stay in Pahalgam Hotel.</p>
+
+<br>
+            <div class="line">
+            </div>
+            <br>
+            <h3 class="day">Day5:</h3>
+            <u>
+                <h4>Pahalgam Sightseeing and Exploration</h4>
+            </u>
+            <p>After the breakfast, visit the scenic and rocky valley of Chandanwadi, Snow Point (around 14 km from
+                Pahalgam) and also enjoy an exciting pony ride.
+
+            <p><b>(Please note, all these activities will be conducted at your own expenses.)</b></p>
+
+            <p> Return to your hotel in the evening. Enjoy the surroundings of Pahalgam on foot.</p>
+
+            <p>Dinner will be served at your hotel.</p>
+
+            <p><b>Important Information :</b> In Pahalgam, outside vehicles are not allowed for sightseeing. Only local taxis can roam around
+                within Pahalgam. All outside vehicles will only be able to offer pick up & drop from the hotel.</p>
+<br>
+            <div class="line">
+            </div>
+            <br>
+            <h3 class="day">Day6:</h3>
+            <u>
+                <h4>Pahalgam to Srinagar Airport Drop (100 Km / Approx. 3½ Hour Drive)</h4>
+            </u>
+
+            <p>
+                After the buffet breakfast, check out from the hotel and drive to Srinagar Airport to board your flight
+                for you onward journey with memorable experiences offered by Tour My India.</p>
+
+            <p><b>Important Information :</b> Due to strict security, you are requested to leave from the hotel at least 4
+                hours before the departure time. You will have to pass 4 security barriers before reaching the airport.
+                This often leads to long traffic jams right outside the airport.</p>
+
+            <p><b>Tour and Services ends</b></p>
+            </p>
+        </div>
+    </section>
+    <form action="">
+        <button type="submit" class="trip">Book Now</button>
+    </form>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -8,85 +8,15 @@
     <title>Mini Project Website</title>
 
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/navigation.css">
 
 </head>
 
 <body>
 
-    <section class="section1">
-        <div class="container1">
-
-            <div class="menu1" text-align="right">
-
-                <ul>
-                    <li>
-                        <img src="images/world-icon.png" alt="">
-                        <a href="#">World Tour</a>
-                    </li>
-
-
-
-                    <li>
-                        <img src="images/time-icon.png" alt="">
-                        <a href="#">9AM-10PM</a>
-                    </li>
-
-
-
-                    <li>
-                        <img src="images/phone-icon.png" alt="">
-                        <a href="#">+91 9172942152</a>
-                    </li>
-                    <li>
-                        <button class="lo"><a href="login.php">LOGIN</a></button>
-                        
-
-                    </li>
-                    <li>
-                        <button class="reg"><a href="register.php">REGISTER</a></button>
-                    </li>
-
-
-
-                </ul>
-
-
-
-            </div>
-
-
-        </div>
-    </section>
-
-
-    <section class="section2">
-        <div class="container2">
-
-            <div class="menu2" text-align="right">
-
-                <ul>
-                    <li>
-                        <b href="#">HOME</b>
-                    </li>
-                    <li>
-                        <b href="#">ABOUT</b>
-                    </li>
-                    <li>
-                        <b href="#">PLANS</b>
-                    </li>
-                    <li>
-                        <b href="#">REVIEWS</b>
-                    </li>
-                    <li>
-                        <b href="#">CONTACT US</b>
-                    </li>
-                </ul>
-
-            </div>
-
-        </div>
-    </section>
-
+<?php 
+    include("navigation.php")
+    ?> 
 
     <section class="section3">
         <div class="container3">
