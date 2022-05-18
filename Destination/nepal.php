ce7300c9d1629feb410a6d5bdf93dcce69cce040
Jatin <jatinbhirud09@gmail.com>
2022-05-18 18:57:32 +0530

    nainitaland3newpages

---
+++ Destination/nepal.php
@@ -0,0 +1,395 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/navigation.css" />
+    <link rel="stylesheet" href="../css/nepal.css">
+</head>
+
+<body>
+    <?php 
+    include("../navigation.php")
+    ?>
+
+    <section class="section1">
+        <div class="container1"></div>
+
+        <div class="container2">
+            <h1>Everest Base Camp Lodge Trek</h1>
+            <h4>15 Nights-16 Days</h4>
+            <div class="line-3">
+                <hr />
+                <h3>Inclusions</h3>
+                <div class="b1">
+                    <div>
+                        <img src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/64/000000/external-hotel-hotel-kiranshastry-gradient-kiranshastry.png"
+                            width="50px" />
+                        <br />
+
+                        <h5>5 star</h5>
+                    </div>
+
+                    <div class="b2">
+                        <img src="https://img.icons8.com/nolan/64/car-roof-box.png" width="50px" />
+                        <br />
+                        <h5>Transfer</h5>
+                    </div>
+
+                    <div class="b3">
+                        <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-dinner-casino-flaticons-lineal-color-flat-icons.png"
+                            width="50px" />
+                        <br />
+                        <h5>Meals</h5>
+                    </div>
+
+                    <div class="b4">
+                        <img src="https://img.icons8.com/external-colours-bomsymbols-/91/000000/external-binoculars-digital-design-colors-set-1-colours-bomsymbols-.png"
+                            width="50px" />
+                        <br />
+                        <h5>Slightseeing</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <br />
+    <section class="section2">
+        <div class="box">
+            <h2 class="overview">Overview</h2>
+            <p>
+                Embark on a beautiful odyssey in Nepal with Tour My India which is filled with an abundance of natural
+                beauty, trekking in the Himalayas, and cultural diversity. The Everest Base Camp Lodge is one of the
+                most scenic treks in the beautiful Sherpa heartland of the Khumbu Valley. This trekking tour in Nepal
+                starts with a sightseeing tour of Kathmandu’s historical and cultural landmarks. For starting two days
+                of this hiking and walking tour, you will be relishing your holidays in Nepal’s cultural and historical
+                hues. However, the trekking experience will begin from day 3. On this Nepal trekking tour, you will get
+                to witness the surreal sights of more than 15 Himalayan peaks including Kusum Kanguru, Pumori, Ama
+                Dablam, Thamserku, Island Peak, Nuptse, Kala Patthar as well as Mount Everest. Savouring the views of
+                these Himalayan mountain peaks is undoubtedly a magical experience which you literally can’t get over
+                for a long time.
+                Being one of the leading tour operators, Tour My India serves some incredible Nepal tour packages that
+                come with amazing services and great hospitality and that too at best prices. So make sure to bookmark
+                this 16D/15N Nepal Everest Base Camp Lodge itinerary an amazing trekking holiday in Nepal.
+                <br>
+        </div>
+    </section>
+    <br>
+    <section class="section3">
+        <div class="box">
+            <h2 class="overview">Itinerary</h2>
+            <h3 class="day">Day1:</h3>
+            <u>
+                <h4> ARRIVAL AT KATHMANDU</h4>
+            </u>
+            <p>
+                Your trekking tour in Nepal with Tour My India starts from Kathmandu Airport where you will be welcomed
+                by our representative. From the airport, you will be accommodated to your hotel. After freshening up,
+                you will be taken on a city sightseeing tour. On the tour, you will visit the two most famous tourist
+                attractions of Kathmandu, Thamel and Kathmandu Durbar Square. Thamel is a commercial hub that houses
+                bakeries, shops, restaurants and hotels. Whereas, the Kathmandu Durbar Square is an old royal palace
+                which is known for housing shrines, temples, and large courtyards. For overnight stay and dinner, you
+                will be taken back to your hotel.
+
+            </p>
+            <br>
+            <div class="line">
+            </div>
+            <br>
+            <h3 class="day">Day2:</h3>
+            <u>
+                <h4> SIGHTSEEING OF KATHMANDU VALLEY</h4>
+            </u>
+            <p>
+
+                In the morning, you will be served with a scrumptious breakfast. Followed by this, be ready to commence
+                a day sightseeing tour of Kathmandu Valley. The tour starts with a pilgrimage to Pashupatinath Temple on
+                the bank of River Bagmati. Thereafter, visit the popular stupa of Boudhanath along with various other
+                Nepali and Tibetan pilgrim sites. Patan would be the last stop of the sightseeing tour where you will
+                get a chance to know about the traditional art of Hindu and Buddhist culture in Nepal. Overnight stay
+                and dinner will be provided at the same hotel.
+
+
+            </p>
+            <br>
+            <div class="line">
+            </div>
+            <br>
+            <h3 class="day">Day3:</h3>
+            <u>
+                <h4> KATHMANDU TO LUKLA TO PHAKDING</h4>
+            </u>
+            <p>
+                The day 3 of this wonderful Nepal tour will start a bit early with a drive to Kathmandu Airport to board
+                a flight to Lukla. Upon arrival at Lukla, you will be served with a delectable breakfast. Followed by
+                this, commence a trek to Phakding through beautiful villages. En route, you will get a chance to witness
+                the Nepal terrace culture. Besides, you can expect to see the surreal views of Kusum Kanguru. After 3-4
+                hour of walking, you will reach Phakding. Overnight stay and dinner will be provided at the campsite at
+                Phakding.
+
+
+            </p>
+            <br>
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day4:</h3>
+            <u>
+                <h4> PHAKDING TO NAMCHE BAZAAR</h4>
+            </u>
+            <p>
+                Followed by a delicious breakfast, you will commence a challenging 6-7hours trek to Namche Bazaar that
+                passes through the west bank of the River Dudh Kosi and the small villages of Toktok. En route, relish
+                the picture-perfect views of Nepal Himalayas including the Thamserku peak. The trek takes you to the
+                village of Benkar. Above the village, there are cliffs that are worth a visit as they are beautifully
+                adorned with the paintings of Buddhist deities. The trek further leads you to Sagarmatha National Park
+                at Monjo. Further traversing the village of Jorsale and the curving ascent of pine forest, you will
+                reach Namche Bazaar in Solukhumbu district. For overnight stay and dinner, you will be accommodated in a
+                hotel in Namche Bazaar.
+
+
+            </p>
+
+            <br>
+
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day5:</h3>
+            <u>
+                <h4> NAMCHE BAZAAR</h4>
+            </u>
+            <p>
+                The day of this wonderful Nepal trekking tour will be free for leisure. After having breakfast, you will
+                be free for your own time. You have the option to either go on a tour to Sherwin Khangba Sherpa Culture
+                Museum or can also hike up to the Hotel Everest View, Kundi Hospital in Khundi Village. Overnight stay
+                and dinner will be provided at the hotel in Namche Bazaar.
+
+
+
+            </p>
+
+            <br>
+
+
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day6:</h3>
+            <u>
+                <h4> NAMCHE BAZAAR TO TENGBOCHE</h4>
+            </u>
+            <p>
+                Followed by a scrumptious breakfast in the morning, be ready to commence a 5-6hours trek to Tengboche.
+                The first half of the trek cuts through winding trails that proffers the scenic views of the Mount
+                Everest, Ama Dablam and Thamserku. The second half of the trek would be 2-hour uphill trekking to
+                Tengboche that passes through rhododendron forests where you can expect to spot Musk Deer and Himalayan
+                Tahr. Tengboche is a village which is known for its Buddhist Tengboche Monastery, the viewpoint and Cafe
+                Tengboche. Overnight stay and dinner will be provided at the campsite/hotel in Tengboche.
+
+
+            </p>
+
+            <br>
+
+
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day7:</h3>
+            <u>
+                <h4>TENGBOCHE TO DINGBOCHE</h4>
+            </u>
+            <p>
+                In the morning, after having breakfast, commence the trek to Dingboche. It is a downhill walk through
+                the forest of rhododendron and conifer along with the small village of Deboche from where you can relish
+                the views of Ama Dablam. The trek also passes through the suspension bridge. Thereafter, the trail then
+                ascent to the village of Pangboche. Passing through numerous pastures, you will reach Dingboche.
+
+
+            </p>
+
+            <br>
+
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day8:</h3>
+            <u>
+                <h4>LEISURE DAY AT DINGBOCHE</h4>
+            </u>
+            <p>
+                Day 8 of this enthralling tour will be free for leisure. After breakfast, you will be free for
+                self-time. You can either trek to the remote village of Chhukung and Chhukung Ri or can just relish the
+                sights of Amphu Lapcha, Island Peak, Baruntse Base Camp and the white ice lake of Imja Tsho. Overnight
+                stay and dinner will be provided at the hotel/camp.
+
+
+
+            </p>
+
+            <br>
+
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day9:</h3>
+            <u>
+                <h4> DINGBOCHE TO LOBUCHE</h4>
+            </u>
+            <p>
+                After having breakfast, be ready to begin a trek to Lobuche. En route, we will halt at the viewpoint
+                where you will get to capture the enticing sights of Cholatse Peak and Tawoche. In the afternoon, we
+                will stop at Dugla for lunch. Thereafter, for an hour, the trek then continues to ascend up the gravely
+                terminal moraine of the Khumbu glacier. It takes you to the memorial monuments of lost sherpas and
+                climbers. Thereafter, the trail then descends towards Lobuche. For the continuous 4-5 hour of walking,
+                you will reach the village of Lobuche. Overnight stay and dinner will be provided at a lodge in Gorak
+                Shep – a quaint settlement at the bottom of Khumbu Icefall. Overnight stay and dinner at Lobuche.
+
+
+            </p>
+
+            <br>
+
+
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day10:</h3>
+            <u>
+                <h4>LOBUCHE TO GORAK SHEP</h4>
+            </u>
+            <p>
+                Post breakfast, start the trek to Gorakshep. This would be a bit challenging trek as it cuts through the
+                steep and rough trails. En route, you will get to catch the sights of Pumori, Nuptse, Kala Patthar,
+                Mount Everest and Khumbu Glacier. By lunchtime, you will be at Gorak Shep. From there, in the afternoon,
+                trek to the base camp. The entire trek would take would take around 5-6 hours of walking. For overnight
+                stay and dinner, you will be accommodated to a camp/hotel.
+
+
+
+            </p>
+
+            <br>
+
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day11:</h3>
+            <u>
+                <h4> GORAK SHEP TO PHERICHE</h4>
+            </u>
+            <p>
+                The day 11 will start early in the morning with a trek to Kala Patthar – the Black Rock. From the peak,
+                you can enjoy a 360-degree view of the Himalayan peaks which includes Lobuche, Pumori, Nuptse, Everest
+                and Lhotse. Thereafter, head back to Gorak Shep for breakfast and followed by that start a trek to
+                Pheriche. Overnight stay and dinner will be provided at the camp/hotel.
+
+
+
+            </p>
+
+            <br>
+
+
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day12:</h3>
+            <u>
+                <h4> PHERICHE TO NAMCHE BAZAAR</h4>
+            </u>
+            <p>
+                In the morning, after having breakfast, you will be asked to be ready with your backpack to commence a
+                trek to Namche Bazaar. It is a 5-hour downhill trek from Pheriche to Namche Bazaar. Dinner and overnight
+                stay will be provided at Namche Bazaar.
+
+
+
+            </p>
+
+            <br>
+
+
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day13:</h3>
+            <u>
+                <h4> NAMCHE BAZAAR TO LUKLA</h4>
+            </u>
+            <p>
+                The day starts with a delectable breakfast, start a 6-hour downhill trek to the Dudh Koshi River. From
+                there, ascend towards Monjo and then further to Benkar, Phakling and Luka. For overnight stay and
+                dinner, you will be accommodated to a lodge/camp at Lukla.
+
+
+            </p>
+
+            <br>
+
+
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day14:</h3>
+            <u>
+                <h4> LUKLA TO KATHMANDU</h4>
+            </u>
+            <p>
+                After serving you with a delectable breakfast, we will transfer you to the airport through a 40-minute
+                drive to board a flight to Kathmandu. Upon arrival, you will be transferred to a pre-booked hotel.
+
+
+
+            </p>
+
+            <br>
+
+
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day15:</h3>
+            <u>
+                <h4> KATHMANDU SIGHTSEEING</h4>
+            </u>
+            <p>
+                On the last day of this wonderful Nepal tour, you will have the opportunity to explore the city. You can
+                pay a visit to the beautiful five-storied Nyatapola Temple, Peacock Window in Bhaktapur, Swayambhunath
+                Stupa, which is also known as the monkey temple and Budhanilkantha. Along with that, you can also go for
+                the last minute shopping in Kathmandu. Overnight stay and dinner will be provided at the same hotel.
+
+
+            </p>
+
+            <br>
+
+
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day16:</h3>
+            <u>
+                <h4> DEPARTURE</h4>
+            </u>
+            <p>Now comes the saddest part of this enthralling tour, departure! As usual, have a delicious breakfast in
+                the morning. Followed by this, be ready with your backpacks as later in the morning we will transfer you
+                to the Kathmandu Airport to board your flight back home. Here, you will conclude this beautiful trip
+                with bucket loads of memories which you will cherish for a long time.
+
+            </p>
+
+            <br>
+
+        </div>
+    </section>
+    <form action="">
+        <button type="submit" class="trip">Book Now</button>
+    </form>
+</body>
+
+</html>
