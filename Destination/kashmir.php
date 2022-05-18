252f27eb60579e9a2f9aa8454c85e427ba38a217
Jatin <jatinbhirud09@gmail.com>
2022-05-18 19:03:47 +0530

    kashmirshift

--- Destination/kashmir.php
+++ Destination/kashmir.php
@@ -6,13 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="stylesheet" href="css/kashmir.css" />
-    <link rel="stylesheet" href="css/navigation.css">
+    <link rel="stylesheet" href="../css/navigation.css">
+    <link rel="stylesheet" href="../css/kashmir.css" />
 </head>
 
 <body>
     <?php 
-    include("navigation.php")
+    include("../navigation.php")
     ?> 
     <section class="section1">
         <div class="container1"></div>
