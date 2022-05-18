a375c54ff313d41f964636da02679b5b37159e58
Mansi Bhondave <mansibhondave@gmail.com>
2022-05-18 17:23:50 +0530

    destinations

---
+++ Destination/hongkong1.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/navigation.css" />
+    <link rel="stylesheet" href="../css/hongkong1.css">
+</head>
+
+<body>
+    <?php 
+    include("../navigation.php")
+    ?>
+
+    <section class="section1">
+        <div class="container1"></div>
+
+        <div class="container2">
+            <h1>Hong Kong Tour with Disneyland</h1>
+            <h4>4 Nights-5 Days</h4>
+            <div class="line-3">
+                <hr />
+                <h3>Inclusions</h3>
+                <div class="b1">
+                    <div>
+                        <img src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/64/000000/external-hotel-hotel-kiranshastry-gradient-kiranshastry.png"
+                            width="50px" />
+                        <br />
+
+                        <h5>5 star</h5>
+                    </div>
+
+                    <div class="b2">
+                        <img src="https://img.icons8.com/nolan/64/car-roof-box.png" width="50px" />
+                        <br />
+                        <h5>Transfer</h5>
+                    </div>
+
+                    <div class="b3">
+                        <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-dinner-casino-flaticons-lineal-color-flat-icons.png"
+                            width="50px" />
+                        <br />
+                        <h5>Meals</h5>
+                    </div>
+
+                    <div class="b4">
+                        <img src="https://img.icons8.com/external-colours-bomsymbols-/91/000000/external-binoculars-digital-design-colors-set-1-colours-bomsymbols-.png"
+                            width="50px" />
+                        <br />
+                        <h5>Slightseeing</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <br />
+    <section class="section2">
+        <div class="box">
+            <h2 class="overview">Overview</h2>
+            <p>
+            There would not be a sight as mind-blowing than visiting a tourism destination that is home to more 
+            than 7000 skyscrapers and many more such marvels. Making its place as a global center of trade, Hong 
+            Kong has swayed away the heart of every type of traveller. And if you are looking for the perfect 
+            Hong Kong vacation package that would suit your preferences the best, then choose Tour My India. As 
+            not only we provide the best holiday experience by our amazing Hong Kong travel itineraries but also 
+            make it a point that you travel with us again and again. In this our 4 nights 5 days Hong Kong holiday 
+            package contain a list of attractions that you will be visiting in Hong Kong and Macau, the famous 
+            Disneyland theme park being one of them. Apart from this, we also take care of your accommodation need 
+            and transportation needs as well. To know more about what all we have to offer, take a look at our 
+            detailed itinerary mentioned below.
+                <br>
+        </div>
+    </section>
+    <br>
+    <section class="section3">
+        <div class="box">
+            <h2 class="overview">Itinerary</h2>
+            <h3 class="day">Day1:</h3>
+            <u>
+                <h4>ARRIVAL AT HONG KONG AIRPORT</h4>
+            </u>
+            <p>
+            Our representative will warmly welcome you at the Hong Kong Airport and will escort you to your hotel.
+            Post your check in you can spend rest of the day your way.
+            </p>
+            <br>
+            <div class="line">
+            </div>
+            <br>
+            <h3 class="day">Day2:</h3>
+            <u>
+                <h4> HONG KONG CITY SIGHTSEEING TOUR</h4>
+            </u>
+            <p>
+            On day 2 we will take you for a half day tour to must visit places in Hong Kong after which the day 
+            is at your disposal. Later proceed to your hotel for night stay.
+            </p>
+            <br>
+            <div class="line">
+            </div>
+            <br>
+            <h3 class="day">Day3:</h3>
+            <u>
+                <h4>HONG KONG-DISNEYLAND</h4>
+            </u>
+            <p>
+            Savour the delectable breakfast at the hotel after which we will proceed to spend some fun time at 
+            the Disneyland, one of the top attractions of Hong Kong. You can take some time to relax post your 
+            check-in at the hotel. Thereafter we will head out to Disneyland theme park followed by night stay at
+            the hotel.
+            </p>
+            <br>
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day4:</h3>
+            <u>
+                <h4> DISNEYLAND - MACAU</h4>
+            </u>
+            <p>
+            After filling your stomach with the scrummy morning meal, you have to check out from the hotel as we 
+            will be proceeding to Hong Kong pier. From the pier we will head to Macau pier via a relaxing ferry 
+            ride and check in at the hotel. Thereafter you can spend some time to relax and unwind as we will 
+            take you for a half day Macau tour. Night stay at the hotel in Macau.
+            </p>
+
+            <br>
+
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day5:</h3>
+            <u>
+                <h4>  DEPARTURE MACAU</h4>
+            </u>
+            <p>
+            Bid goodbye to Hong Kong as its time to catch your flight home or to the next travel destination.
+            </p>
+
+            <br>
+
+        </div>
+    </section>
+    <form action="">
+        <button type="submit" class="trip">Book Now</button>
+    </form>
+</body>
+
+</html>
---
+++ Destination/sikkim.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/navigation.css" />
+    <link rel="stylesheet" href="../css/sikkim.css">
+</head>
+
+<body>
+    <?php 
+    include("../navigation.php")
+    ?>
+
+    <section class="section1">
+        <div class="container1"></div>
+
+        <div class="container2">
+            <h1>NORTH-EAST INDIA HONEYMOON TOUR</h1>
+            <h4>5 Nights-6 Days</h4>
+            <div class="line-3">
+                <hr />
+                <h3>Inclusions</h3>
+                <div class="b1">
+                    <div>
+                        <img src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/64/000000/external-hotel-hotel-kiranshastry-gradient-kiranshastry.png"
+                            width="50px" />
+                        <br />
+
+                        <h5>5 star</h5>
+                    </div>
+
+                    <div class="b2">
+                        <img src="https://img.icons8.com/nolan/64/car-roof-box.png" width="50px" />
+                        <br />
+                        <h5>Transfer</h5>
+                    </div>
+
+                    <div class="b3">
+                        <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-dinner-casino-flaticons-lineal-color-flat-icons.png"
+                            width="50px" />
+                        <br />
+                        <h5>Meals</h5>
+                    </div>
+
+                    <div class="b4">
+                        <img src="https://img.icons8.com/external-colours-bomsymbols-/91/000000/external-binoculars-digital-design-colors-set-1-colours-bomsymbols-.png"
+                            width="50px" />
+                        <br />
+                        <h5>Slightseeing</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <br />
+    <section class="section2">
+        <div class="box">
+            <h2 class="overview">Overview</h2>
+            <p>
+            North East India is famous for its exotic honeymoon destinations where the two souls can become one 
+            and have a blast of a life time. With adoring surroundings, gusty winds and soothing ambience, the 
+            fascinating hill stations of the North East India offer much-required privacy to honeymooners so that 
+            they have the most romantic retreat. For all those who are planning their honeymoon right in the 
+            nature’s lap, we offer an exclusive 5 nights / 6 days North East Honeymoon Tour. Be ready to explore 
+            the beautiful sightseeing destinations of North East India including Kalimpong, Gangtok and Darjeeling 
+            with us and take home many sweet and unforgettable memories to cherish forever.
+            <br>
+        </div>
+    </section>
+    <br>
+    <section class="section3">
+        <div class="box">
+            <h2 class="overview">Itinerary</h2>
+            <h3 class="day">Day1:</h3>
+            <u>
+                <h4>BAGDOGRA AIRPORT / NJP RAILWAY STATION - KALIMPONG (77 KMS / 3 HRS)</h4>
+            </u>
+            <p>
+            Upon arrival at Bagdogra Airport / NJP Railway Station meeting and assistance followed by transfer to 
+            Kalimpong. On arrival Check-in to your hotel. Rest of the day free. Overnight in Kalimpong.<br><br>
+
+            About Kalimpong: Kalimpong is a hill station in the Mahabharat Range in the Indian state of West 
+            Bengal. It is located at an average elevation of 1,250 metres. The town is the headquarters of the 
+            Kalimpong subdivision, a part of the district of Darjeeling.
+            </p>
+            <br>
+            <div class="line">
+            </div>
+            <br>
+            <h3 class="day">Day2:</h3>
+            <u>
+                <h4> KALIMPONG - GANGTOK (ABOUT 80 KMS / 3 HRS)</h4>
+            </u>
+            <p>
+            Post breakfast embark on a sightseeing tour of Kalimpong covering - Mangal Dham, Deolo Hill, 
+            Dr. Graham's Home, Durbin Dara Monastery & View point, Cactus / Orchid Nursery. Return to hotel. 
+            Post lunch drive to to Gangtok - "The Capital of Sikkim". Check-in to your hotel on arrival. 
+            Overnight in Gangtok.<br><br>
+
+            Mangal Dham: This ungainly modern Mangal Dham is sacred to Krishna. In its ballroom-like prayer hall 
+            there are eight vibrant, life-size dioramas from the Krishna Leela. The temple is dedicated to Guruji 
+            Shri Mangaldas Ji, who's commemorated in a shrine below the prayer hall. The temple is about 500m 
+            downhill from Thongsa Gompa, or you can walk from the centre along Relli Rd and turn left by the 
+            Roman Catholic church.
+            <br><br>
+            Deolo Hill: Deolo Hill, located to the north-east of the town, is one of the two hills in between 
+            which the town of Kalimpong stands. From the top of this hill, tourists can enjoy beautiful views 
+            of the surrounding villages of Relli valley and Teesta River along with the town of Kalimpong. In 
+            addition, there is a park located at the summit of this hill, which is constructed for recreational 
+            purposes.
+            </p>
+            <br>
+            <div class="line">
+            </div>
+            <br>
+            <h3 class="day">Day3:</h3>
+            <u>
+                <h4>GANGTOK - SIGHTSEEING</h4>
+            </u>
+            <p>
+            After breakfast embark on a half day local sightseeing tour of Gangtok covering Rumtek Monastery, 
+            Dro-Dul Chorten, Institute of Tibetology, Handicraft Centre, and Flower Show. Overnight in Gangtok.
+            <br><br>
+            Institute of Tibetology: Since its establishment in 1958, the Namgyal Institute of Tibetology has 
+            sponsored and promoted research on the religion, history, language, art and culture of the people of 
+            the Tibetan cultural area which includes Sikkim. The NIT library holds one of the largest collections 
+            of Tibetan works in the world outside Tibet and a museum of Tibetan iconography and religious art. It 
+            has published the Bulletin of Tibetology since 1964 and numerous books over the years.
+            <br><br>
+            Handicraft Center: The Directorate of Handicrafts and Handloom was founded in 1957, as Handloom and 
+            Handicrafts Centre of Sikkim. This is situated 500 metres from the central market of Gangtok. This 
+            organization has been established to conserve and pass around the art and craft skills of the 
+            artificers of Gangtok. In this centre, the visitors can see blankets, elegant hand-carved folding 
+            tables (Choktse), hand-loomed carpets with conventional themes, hand-painted masques, shawls, 
+            traditional paintings, wooden dolls and many other handicrafts.
+            </p>
+            <br>
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day4:</h3>
+            <u>
+                <h4> GANGTOK - DARJEELING (APPROX - 110 KMS / 4 HRS)</h4>
+            </u>
+            <p>
+            After breakfast take the transfer to Darjeeling 'The Queen of the Hills'. Check-in to your hotel. 
+            Overnight in Darjeeling.
+            </p>
+
+            <br>
+
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day5:</h3>
+            <u>
+                <h4> DARJEELING - SIGHTSEEING</h4>
+            </u>
+            <p>
+            After breakfast embark on a sightseeing tour of Darjeeling covering - Himalayan Mountaineering 
+            Institute, Padmaja Naidu Himalayan Zoological Park (closed on Thursday), Tenzing Rock, Tibetan 
+            Refugee self help Centre (closed on Sunday), Tea Garden (outer view), and Japanese Peace Pagoda. 
+            Return to hotel. Post Lunch free or at own. Evening can be spent at the Mall (chowrasta). Overnight 
+            in Darjeeling.<br><br>
+
+            Tea Garden: According to records, the first commercial tea gardens planted out by the British tea 
+            interest were Tukvar, Steinthal and Aloobari tea estates. This was in 1852 and this entire plantation 
+            used seeds that were raised in the government nurseries.<br><br>
+
+            Darjeeling was then only a sparsely populated hamlet which was being used as a hill resort by the 
+            army and some affluent people. Tea, being a labor intensive enterprise, required sufficient number 
+            of workers to plant, tend, pluck and finally manufacture the produce. For this, employment was 
+            offered to people from across the border of Nepal.<br><br>
+
+            Tenzing Rock: Tenzing Rock, a gigantic natural rock in Darjeeling is named after Tenzing Norgay, the 
+            man who created history by climbing the Everest first time along with Edmund Hillary in 1953.<br><br>
+
+            Mall: Mall is like the town center or the social center of Darjeeling. This is where some of the old 
+            and heritage stores are lined up on one side, and there is open view on the other with many pine 
+            trees all around. If you want to feel the real vibe of Darjeeling, watch people and activities, this 
+            is the place to come. Another thing that you won't miss here. there are lots of young local lads with 
+            their ponies offering horse rides to children. Many elderly tourists indulge as well.
+            </p>
+
+            <br>
+            <div class="line">
+                <br>
+            </div>
+            <h3 class="day">Day6:</h3>
+            <u>
+                <h4> DARJEELING - BAGDOGRA AIRPORT / NJP RAILWAY STATION (80 KMS / 3 HRS)</h4>
+            </u>
+            <p>
+            After breakfast drive to Bagdogra. On arrival transfer to airport / Railway station to connect 
+            flight / train for onward destination.
+
+
+            </p>
+
+            <br>
+
+        </div>
+    </section>
+    <form action="">
+        <button type="submit" class="trip">Book Now</button>
+    </form>
+</body>
+
+</html>
