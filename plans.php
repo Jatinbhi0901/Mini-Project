e503dcb94f4a571f83b0ef3649e36337e8655ede
Mansi Bhondave <mansibhondave@gmail.com>
2022-05-19 19:20:23 +0530

    created php files

---
+++ plans.php
@@ -0,0 +1,142 @@
+<section class="section5" id="2">
+        <div class="container5">
+
+            <h2>Tour Plans</h2>
+
+            <div class="box">
+
+                <a href="Destination/adventuretour.php"><img src="images/Adventure.jpg" alt="qwe" width="350px"height="200px"/></a>
+
+                <h3>Adventurous Tour</h3>
+
+           
+            </div>
+
+
+
+
+            <div class="box">
+                <a href="Destination/beachestour.php"><img src="images/b.jpg" alt="qwe" width="350px"height="200px"></a>
+                <h3>Beach</h3>
+
+            </div>
+
+
+
+
+            <div class="box">
+                <a href="Destination/honeymoontour.php"><img src="images/honeymoon.jpg" alt="qwe"width="350px" height="200px"></a>
+                <h3>Honeymoon Tour</h3>
+
+            </div>
+
+
+
+
+            <div class="box">
+                <a href="Destination/family.php"><img src="images/family.jpg" alt="qwe"width="350px"height="200px"></a>
+                <h3>Family Tour</h3>
+
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </section>
+
+
+
+
+    <section class="section6" id="3">
+        <div class="container6">
+            <f href="#">Create Your own Plans</f><br><br>
+            <div class="find">
+                <form action="" method="post">
+                    <input type="search" name="search" placeholder="Choose Your Favourite Destination" >
+                    <input type="submit" name="submit" value="Search" class="btn1">
+                </form>
+                
+            </div>
+
+        </div>
+    </section>
+
+
+
+
+    <section class="section7" id="4">
+        <div class="container7">
+            <h2>World Tour</h2>
+
+            <div class="world">
+                <p>
+                <div class="qwer">
+                    <img src="images/switzerland/p1.jpg" alt="qwe" width="400px" height="280px">
+                </div><br>
+                <h3> EUROPE</h3>
+                </p>
+            </div>
+
+            <div class="world">
+                <p>
+                <div class="qwer">
+                    <img src="images/australia/a2.jpg" alt="qwe" width="400px" height="280px">
+                </div>
+                <br>
+                <h3> AUSTRALIA</h3>
+                </p>
+            </div>
+
+
+            <div class="world">
+                <p>
+                <div class="qwer">
+                    <img src="images/bangkok.jpg" alt="qwe"  width="400px" height="280px">
+                </div>
+                <br>
+                <h3> BANGKOK</h3>
+                </p>
+            </div>
+            
+
+
+            <div class="world">
+                <p>
+                <div class="qwer">
+                    <img src="images/singpore.jfif " alt="qwe" width="400px" height="280px">
+                </div>
+                <br>
+                <h3> SINGAPORE</h3>
+                </p>
+            </div>
+
+
+            <div class="world">
+                <p>
+                <div class="qwer">
+                    <img src="images/dubai.jpeg" alt="qwe"  width="400px" height="280px">
+                </div>
+                <br>
+                <h3> DUBAI</h3>
+                </p>
+            </div>
+
+
+            <div class="world">
+                <p>
+                <div class="qwer">
+                    <img src="images/india.jpeg " alt="qwe" width="400px" height="280px">
+                </div>
+                <br>
+                <h3> INDIA</h3>
+                </p>
+            </div>
+
+
+
+
+
+
+            <div class="clearfix"></div>
+
+
+        </div>
+    </section>
