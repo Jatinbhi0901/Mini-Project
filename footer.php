d25090b6355491c88aaec27ebe782dfc99f07266
Mansi Bhondave <mansibhondave@gmail.com>
2022-05-19 19:59:45 +0530

    edited footer

--- footer.php
+++ footer.php
@@ -13,29 +13,34 @@
            <table class="table">
                <tr>
                    <th class="up">
-                       Famous Tours
+                       Quick Links
                    </th>
                </tr>
                <tr>
                    <td>
-                    <a href="" class="click">Maldives</a>   
+                    <a href="Destination/maldives.php" class="click">Maldives</a>   
                     </td>
                </tr>
                
                <tr>
                     <td >
-                        <a href="" class="click">Singapore</a>
+                        <a href="Destination/switzerland2.php" class="click">Switzerland</a>
                     </td>
                 </tr>
 
                 <tr>
                     <td >
-                        <a href="" class="click">Dubai</a>
+                        <a href="Destination/hongkong1.php" class="click">Hongkong</a>
                     </td>
                 </tr>
                 <tr>
                     <td >
-                        <a href="" class="click">India</a>
+                        <a href="Destination/nepal.php" class="click">Nepal</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td >
+                        <a href="Destination/bali.php" class="click">Bali</a>
                     </td>
                 </tr>
            </table>
@@ -49,24 +54,30 @@
             </tr>
             <tr>
                 <td >
-                    <a href="" class="click">India</a>
+                    <a href="Destination/indiatour.php" class="click">India</a>
                 </td>
             </tr>
             
             <tr>
                  <td >
-                     <a href="" class="click">Europe</a>
+                     <a href="Destination/europetour.php" class="click">Europe</a>
                  </td>
              </tr>
 
              <tr>
+                <td >
+                    <a href="Destination/australiatour.php" class="click">Australia</a>
+                </td>
+            </tr>
+
+             <tr>
                  <td >
-                     <a href="" class="click">Singapore</a>
+                     <a href="Destination/singaporetour.php" class="click">Singapore</a>
                  </td>
              </tr>
              <tr>
                  <td >
-                     <a href="" class="click">Dubai</a>
+                     <a href="Destination/dubaitour.php" class="click">Dubai</a>
                  </td>
              </tr>
         </table>
@@ -74,29 +85,36 @@
         <table class="table">
             <tr>
                 <th class="up">
-                    About Us
+                    Legacy Policy
                 </th>
             </tr>
             <tr>
                 <td >
-                    <a href=""class="click">Our Service</a>
+                    <a href=""class="click">Terms & Condition</a>
                 </td>
             </tr>
             
             <tr>
                  <td >
-                     <a href=""class="click">Contact</a>
+                     <a href=""class="click">Privacy Policy</a>
                  </td>
              </tr>
 
              <tr>
+                <td >
+                    <a href=""class="click">Disclamier</a>
+                </td>
+            </tr>
+            <tr>
+
+             <tr>
                  <td >
-                     <a href=""class="click">Privacy Policy</a>
+                     <a href=""class="click">About Us</a>
                  </td>
              </tr>
              <tr>
                  <td >
-                     <a href=""class="click">Feedback</a>
+                     <a href=""class="click">Contact Us</a>
                  </td>
              </tr>
         </table>
@@ -109,25 +127,19 @@
             </tr>
             <tr>
                 <td >
-                    <a href=""class="click">Worldwide Service</a>
+                    <a href=""class="click">Positive Customer Feedback</a>
                 </td>
             </tr>
             
             <tr>
                  <td >
-                     <a href=""class="click">More Than 1000 Branches</a>
-                 </td>
-             </tr>
-
-             <tr>
-                 <td >
-                     <a href=""class="click">All SServices</a>
+                     <a href=""class="click">More Than 20 years of Experience</a>
                  </td>
              </tr>
 
              <tr>
                 <td >
-                    <a href="" class="click">Yarr yeh section likh dena</a>
+                    <a href="" class="click">Accredited by major tour & travel association</a>
                 </td>
             </tr>
         </table>
